50caf981b3d39d92925187e19c3aa20a16d49158
DannisonArias <dannisonarias@gmail.com>
2020-04-07 17:20:31 -0400

    add remember token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_185512) do
+ActiveRecord::Schema.define(version: 2020_04_07_211242) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_185512) do
     t.integer "membership"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
